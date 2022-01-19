706651c8a69b8f9256d23d471f8d1af8048ac3ce
Szabó Dániel Ernő <r3ap3rpy@gmail.com>
2022-01-19 07:17:19 +0100

    Create staetefull.py

---
+++ staetefull.py
@@ -0,0 +1,17 @@
+from diagrams import Cluster, Diagram
+
+from diagrams.k8s.compute import Pod, StatefulSet
+from diagrams.k8s.network import Service
+from diagrams.k8s.storage import PV, PVC, StorageClass
+
+with Diagram("Stateful"):
+	with Cluster("Apps"):
+		svc = Service("svc")
+		sts = StatefulSet("sts")
+		apps = []
+		for _ in range(5):
+			pod = Pod("pod")
+			pvc = PVC("pvc")
+			pod - sts - pvc
+			apps.append(svc >> pod >> pvc)
+	apps  << PV("pv") << StorageClass("sc")
